df1c6b736b030c7df3733a63e5986479c9c2ada1
ahogappa <ahogappa@st.inc>
2025-05-16 13:16:28 +0900

    fix up

--- lib/taski.rb
+++ lib/taski.rb
@@ -18,64 +18,125 @@ module Taski
   end
 
   class Task
-    def self.__resolve__
-      @__resolve__ ||= {}
-    end
+    class << self
+      def ref(klass)
+        ref = Reference.new(klass)
+        throw :unresolved, ref
+      end
 
-    def self.ref(klass)
-      ref = Reference.new(klass)
-      throw :unresolve, ref
-    end
+      def definition(name, block, **options)
+        @dependencies ||= []
+        @definitions ||= {}
 
-    def self.definition(name, block, **options)
-      @dependencies ||= []
-      @definitions ||= {}
+        class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
+          def self.#{name}
+            __resolve__[__callee__] ||= false
+            if __resolve__[__callee__]
+              # already resolved
+            else
+              __resolve__[__callee__] = true
+              throw :unresolved, [self, __callee__]
+            end
+          end
+        RUBY
 
-      self.class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
-        def self.#{name}
-          __resolve__[__callee__] ||= false
-          if __resolve__[__callee__]
-            # already resolved
+        classes = []
+        loop do
+          klass, task = catch(:unresolved) do
+            block.call
+            nil
+          end
+
+          if klass.nil?
+            classes.each do |task_class|
+              task_class[:klass].class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
+                __resolve__ = {}
+              RUBY
+            end
+
+            break
           else
-            __resolve__[__callee__] = true
-            throw :unresolve, [self, __callee__]
+            classes << { klass:, task: }
           end
         end
-      RUBY
 
-      classes = []
-      loop do
-        klass, task = catch(:unresolve) do
-          block.call
-          nil
-        end
+        @dependencies += classes
+        @definitions[name] = { block:, options:, classes: }
+      end
 
-        if klass.nil?
-          classes.each do |task_class|
-            task_class[:klass].class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
-              __resolve__ = {}
-            RUBY
-          end
-
-          break
-        else
-          classes << { klass:, task: }
+      def build
+        resolve_dependencies.reverse.each do |task_class|
+          task_class.new.build
         end
       end
 
-      @dependencies += classes
-      @definitions[name] = {block:, options:, classes:}
-    end
-
-    def self.build
-      resolve_dependences.reverse.each do |task_class|
-        task_class.new.build
+      def clean
+        resolve_dependencies.each do |task_class|
+          task_class.new.clean
+        end
       end
-    end
 
-    def self.clean
-      resolve_dependences.each do |task_class|
-        task_class.new.clean
+      def refresh
+        # TODO
+      end
+
+      private
+
+      def __resolve__
+        @__resolve__ ||= {}
+      end
+
+      def resolve(queue, resolved)
+        @dependencies.each do |task|
+          if task[:klass].is_a?(Reference)
+            task[:klass].deref
+          else
+            task[:klass]
+          end => task_class
+
+          # increase priority
+          if resolved.include?(task_class)
+            resolved.delete(task_class)
+          end
+          queue << task_class
+        end
+
+        # override
+        class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
+          def self.ref(klass)
+            Object.const_get(klass)
+          end
+        RUBY
+
+        @definitions.each do |name, (_block, _options)|
+          # override
+          class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
+            def self.#{name}
+              @__#{name} ||= @definitions[:#{name}][:block].call
+            end
+          RUBY
+
+          define_method(name) do
+            unless instance_variable_defined?("@__#{name}")
+              instance_variable_set("@__#{name}", self.class.send(name))
+            end
+            instance_variable_get("@__#{name}")
+          end
+        end
+
+        self
+      end
+
+      def resolve_dependencies
+        queue = [self]
+        resolved = []
+
+        while queue.any?
+          resolved << task_class = queue.shift
+          task_class.resolve(queue, resolved)
+        end
+
+        resolved
       end
     end
 
@@ -93,63 +154,6 @@ module Taski
       else
         system command, exception: true
       end
-    end
-
-    def self.refresh
-      # TODO
-    end
-
-    private
-
-    def self.resolve(queue, resolved)
-      @dependencies.each do |task|
-        if task[:klass].is_a?(Reference)
-          task_class = task[:klass].deref
-        end
-
-        # increase priority
-        if resolved.include?(task[:klass])
-          resolved.delete(task[:klass])
-        end
-        queue << task[:klass]
-      end
-
-      # override
-      self.class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
-        def self.ref(klass)
-          Object.const_get(klass)
-        end
-      RUBY
-
-      @definitions.each do |name, (block, options)|
-        # override
-        self.class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
-          def self.#{name}
-            @__#{name} ||= @definitions[:#{name}][:block].call
-          end
-        RUBY
-
-        self.define_method(name) do
-          unless instance_variable_defined?("@__#{name}")
-            instance_variable_set("@__#{name}", self.class.send(name))
-          end
-          instance_variable_get("@__#{name}")
-        end
-      end
-
-      self
-    end
-
-    def self.resolve_dependences
-      queue = [self]
-      resolved = []
-
-      while queue.any?
-        resolved << task_class = queue.shift
-        task_class.resolve(queue, resolved)
-      end
-
-      resolved
     end
   end
 end
